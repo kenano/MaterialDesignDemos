f218fb38b753b9efa2c180dda0bb29b9929446ec
Kenan Ozdamar <kenan.ozdamar@turner.com>
2017-07-09 23:37:43 -0400

    initial commit.

---
+++ RecyclerViewDemo1/app/src/main/java/com/example/kenanozdamar/recyclerviewdemo1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.kenanozdamar.recyclerviewdemo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements Toolbar.OnMenuItemClickListener {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Home Page");
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        switch (id) {
+//            case R.id.linearViewHorizontal:
+//                LinearLayoutManager mLinearLayoutManagerHorizontal = new LinearLayoutManager(this);
+//                mLinearLayoutManagerHorizontal.setOrientation(LinearLayoutManager.HORIZONTAL);
+//                recyclerView.setLayoutManager(mLinearLayoutManagerHorizontal);
+//                break;
+//
+//            case R.id.linearViewVertical:
+//                LinearLayoutManager mLinearLayoutManagerVertical = new LinearLayoutManager(this);
+//                mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+//                recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
+//                break;
+//            case R.id.gridView:
+//                GridLayoutManager mGridLayoutManager = new GridLayoutManager(this, 2); // (Context context, int spanCount)
+//                recyclerView.setLayoutManager(mGridLayoutManager);
+//                break;
+//            case R.id.staggeredViewHorizontal:
+//                StaggeredGridLayoutManager mStaggeredHorizontalLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.HORIZONTAL); // (int spanCount, int orientation)
+//                recyclerView.setLayoutManager(mStaggeredHorizontalLayoutManager);
+//                break;
+//            case R.id.staggeredViewVertical:
+//                StaggeredGridLayoutManager mStaggeredVerticalLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL); // (int spanCount, int orientation)
+//                recyclerView.setLayoutManager(mStaggeredVerticalLayoutManager);
+//                break;
+//        }
+        return true;
+    }
+}
---
+++ RecyclerViewDemo1/app/src/main/java/com/example/kenanozdamar/recyclerviewdemo1/model/NatureVista.java
@@ -0,0 +1,8 @@
+package com.example.kenanozdamar.recyclerviewdemo1.model;
+
+/**
+ * Created by kenanozdamar on 7/9/17.
+ */
+
+public class NatureVista {
+}
